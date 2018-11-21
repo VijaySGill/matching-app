5b7cf2531bdc8f5bdba13ca0bd71983b56b58c70
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-11-21 19:20:39 +0000

    refactored views to check for authentication

--- matchingapp/urls.py
+++ matchingapp/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authenticate/', views.authenticate, name='authenticate'),
+    path('loginPage/', views.loginPage, name='loginPage'),
     path('registerUser/', views.registerUser, name='registerUser'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
     path('getHobbies/', views.getHobbies, name='getHobbies'),
 
